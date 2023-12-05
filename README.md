029eb3815ea250dc2480764e4b34f7ea3e13409a
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-12-05 12:27:37 -0300

    feat: Updated README.md, added new logging info, and improved error handling
    - Added logging information to track network requests and processing time for each name.
    - Implemented handling for empty names in the list to prevent JSON saving errors.
    - Added KeyboardInterrupt exception for program interruption, ensuring saving of 'INCI_results.json' and 'current_index.txt'.
    - Modified saving system to save 'INCI_results.json' every 50 increments and a final save after the entire loop is done.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# cosmile-europe-data-scraping
- 
+# COSMILE Europe Data Scraping Project
+
+## Overview
+
+This project aims to scrape data from the COSMILE Europe website, specifically collecting information on nearly 30,000 ingredients. The ultimate deliverable is an Excel file tabulating the data in a specified format.
+
+## Task Description
+
+The client requests data on ingredients listed on the COSMILE Europe website. The ingredients are accessible only through the search bar, and there are approximately 30,000 of them. The data is tabulated in two formats (examples provided).
+
+- **Example 1:**
+  ```plaintext
+  GLYCERIN
+  Ingredient - COSMILE Europe
+  Data Example 1
+  ```
+
+- **Example 2:**
+  ```plaintext
+  AMP-ISOSTEAROYL HYDROLYZED SOY PROTEIN
+  Ingredient - COSMILE Europe
+  Data Example 2
+  ```
--- initial.py
+++ initial.py
@@ -11,10 +11,12 @@ logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(level
 with open('INCI_names.json', 'r') as f:
     names = json.load(f)
 
+# Just in case a error stop the program
 # Load the last processed index
 with open('current_index.txt', 'r') as f:
     start_index = int(f.read().strip())
 
+# Just in case a error stop the program
 # Load the existing results
 with open('INCI_results.json', 'r') as f:
     results = json.load(f)
@@ -28,60 +30,97 @@ base_url = 'https://cosmileeurope.eu/inci/results/?q='
 # Set the starting index to 0
 start_index = 0
 
+# If you want to start from 0
 # List to store the results
 results = []
 
-# For each name
-for i, name in enumerate(names[start_index:], start=start_index):
-    # If the index is greater than 20, break the loop
-    #if i > 20:
-    #    break    
-    
-    # Split the name by spaces and use the first part for the URL
-    first_word = name.split(' ')[0]
+# User-Agent String, trying to bypass the RemoteDisconnected
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
-    # Make a GET request to the search URL
-    for _ in range(5):  # Retry up to 5 times
-        try:
-            #User-Agent String, trying to bypass the RemoteDisconnected
-            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-            response = session.get(base_url + first_word, headers=headers)
-            break
-        except requests.exceptions.ConnectionError as e:
-            logging.error(f'Connection error at index {i}: {e}')
-            time.sleep(10)  # Wait for 10 seconds before retrying
-    else:
-        logging.error(f'Failed to get response after 5 retries at index {i}')
-        continue
-    
-    # Make a GET request to the search URL
-    #response = session.get(base_url + first_word)
+# Making a failproof for KeyboardInterrupt = Ctrl + C, this is done because i've interrupted one time and i got a error when trying to restart the program!
+try:
+    # For each name
+    for i, name in enumerate(names[start_index:], start=start_index):
+        # If the index is greater than 20, break the loop
+        #if i > 20:
+        #    break    
+        # In Index 2337 got a error: AttributeError: 'NoneType' object has no attribute 'split'
+        if name is None:
+            continue
 
-    # Find all elements that match the CSS selector
-    elements = response.html.find('.inci_box_link-link')
+        # Record the start time
+        start_time = time.time()
 
-    # For each element
-    for element in elements:
-        # Extract the href and text
-        url = element.attrs['href']
-        text = element.text
 
-        # Store the href and text in a dictionary
-        result = {'name': text, 'url': url}
+        # Split the name by spaces and use the first part for the URL
+        first_word = name.split(' ')[0]
 
-        # Append the dictionary to the list of results
-        results.append(result)
 
-    # Log the current index
-    logging.info(f'Processed name at index {i}')
+        # Make a GET request to the search URL
+        for _ in range(5):  # Retry up to 5 times
+            try:
+                logging.info('Starting network request...')
+                response = session.get(base_url + first_word, headers=headers)
+                logging.info('Finished network request.')
+                break
+            except requests.exceptions.ConnectionError as e:
+                logging.error(f'Connection error at index {i}: {e}')
+                time.sleep(10)  # Wait for 10 seconds before retrying
+        else:
+            logging.error(f'Failed to get response after 5 retries at index {i}')
+            continue
+        
+        # Make a GET request to the search URL
+        #response = session.get(base_url + first_word)
 
-    # Save the current index to a separate file
+        # Find all elements that match the CSS selector
+        elements = response.html.find('.inci_box_link-link')
+
+        # For each element
+        for element in elements:
+            
+            # Extract the href and text
+            url = element.attrs['href']
+            text = element.text
+
+            # Store the href and text in a dictionary
+            result = {'name': text, 'url': url}
+
+            # Append the dictionary to the list of results
+            results.append(result)
+            
+            # Changing location to outer loop, to prevent opening and saving the file every iteration
+            # Save the list of results to a new JSON file
+            #with open('INCI_results.json', 'w') as f:
+            #    json.dump(results, f)
+
+        # Save the list of results to a new JSON file
+        # If i is a multiple of 50, save the results to a file
+        if i % 50 == 0:
+            with open('INCI_results.json', 'w') as f:
+                json.dump(results, f)
+
+        # Calculate the processing time
+        processing_time = time.time() - start_time  
+
+        # Log the current index and processing time
+        logging.info(f'Processed name at index {i} in {processing_time} seconds')
+
+        # Save the current index to a separate file
+        with open('current_index.txt', 'w') as f:
+            f.write(str(i))
+
+        # Add a delay to avoid making too many requests in a short time
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    # Save the current state before exiting
+    with open('INCI_results.json', 'w') as f:
+        json.dump(results, f)
     with open('current_index.txt', 'w') as f:
         f.write(str(i))
+    print("Interrupted by user, saved current state.")
 
-    # Add a delay to avoid making too many requests in a short time
-    time.sleep(1)
-
-# Save the list of results to a new JSON file
+# After the loop ends, save the results one last time, made this one because of the "i % 50 == 0" inside the loop
 with open('INCI_results.json', 'w') as f:
     json.dump(results, f)
