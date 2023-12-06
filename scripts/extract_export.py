3028d18e7400fe53d9278b7f529dad7ceba44dcf
Allan Gadelha <c.allan.gadelha@gmail.com>
2023-12-06 18:47:50 -0300

    feat: Update extract_export.py to handle Dynamic Structure of the Website
    Adjusted extract_export.py to accommodate the dynamic structure of the website. Initial output of the data is now stored in INCI_info.csv.

--- scripts/extract_export.py
+++ scripts/extract_export.py
@@ -34,8 +34,8 @@ try:
     for i, url in enumerate(urls[start_index:], start=start_index):
         
         #Testing to see if the code work till the first save breakpoint
-        if i > 102:
-            break
+        #if i > 52:
+        #    break
 
 
         # Just in case there's no url, but we already made sure that everyone has
@@ -62,21 +62,56 @@ try:
         # Parse the HTML response using lxml
         root = etree.HTML(response.content)
 
-        # Find the <h2> and <p> elements using XPath
-        h2_elements = root.xpath('//div[@class="inci_db"]/h2')
-        p_elements = root.xpath('//div[@class="inci_db"]/p')
+        # Find the <h2>, <h3> and <p> elements using XPath
+        #elements = root.xpath('//div[@class="inci_db"]/*[self::h2 or self::h3 or self::p]')
+
+        # Find the <h2>, <h3>, <p> and <div> elements using XPath
+        elements = root.xpath('//div[@class="inci_db"]/*[self::h2 or self::h3 or self::p or self::div[@class="inci_box_links"]]')
 
         # Initialize a dictionary to store the information
         info = {}
+        current_h2 = None
+        current_h3 = None
 
-        # For each <h2> and corresponding <p>
-        for h2, p in zip(h2_elements, p_elements):
-            # Get the text of the <h2> and <p> elements
-            h2_text = h2.text
-            p_text = p.text
+        # For each element
+        for element in elements:
+            # Get the text of the element
+            text = element.text
 
-            # Store the text in the dictionary
-            info[h2_text] = p_text
+            # If the element is <h2>, store the text as a key
+            if element.tag == 'h2':
+                current_h2 = text
+                current_h3 = None
+                info[current_h2] = {}
+            # If the element is <h3>, store the text as a nested key under the current <h2>
+            elif element.tag == 'h3':
+                current_h3 = text
+                info[current_h2][current_h3] = []
+            # If the element is <p>, append the text to the current key
+            elif element.tag == 'p':
+                if current_h3 is not None:
+                    info[current_h2][current_h3].append(text)
+                else:
+                    if 'p' not in info[current_h2]:
+                        info[current_h2]['p'] = []
+                    info[current_h2]['p'].append(text)
+            # If the element is <div> with class 'inci_box_links', get the text from all nested <div> with class 'inci_box_link'
+            #elif element.tag == 'div' and 'inci_box_links' in element.attrib.get('class', ''):
+            #    link_texts = element.xpath('.//div[@class="inci_box_link"]/text()')
+            #    info[current_h2]['inci_box_links'] = link_texts
+            # If the element is <div> with class 'inci_box_links', get the text from all nested <a> with class 'inci_box_link-link'
+            elif element.tag == 'div' and 'inci_box_links' in element.attrib.get('class', ''):
+                link_texts = element.xpath('.//div[@class="inci_box_link"]/div[@class="inci_box_link-content"]/a[@class="inci_box_link-link"]/text()')
+                info[current_h2]['inci_box_links'] = [text.strip() for text in link_texts if text.strip()]  # remove empty strings and strip leading/trailing whitespaces
+
+        # Convert lists of one item to single values
+        for h2, value in info.items():
+            if isinstance(value, dict):
+                for h3, p_list in value.items():
+                    if len(p_list) == 1:
+                        info[h2][h3] = p_list[0]
+            elif isinstance(value, list) and len(value) == 1:
+                info[h2] = value[0]
 
         # Add the "Name" and "Url" to the dictionary
         info['Name'] = url['name']
